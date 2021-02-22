607f461115369563543eb6b661856b6ba92a0513
jnjambi <60658990+jnjambi@users.noreply.github.com>
2021-02-22 11:23:44 +0300

    Add files via upload

---
+++ stock price web-app.py.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and **volume** of Microsoft.
+
+""")
+
+tickerSymbol = "MSFT"
+
+#get data of this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period= '1d', start= '2010-5-31', end= '2020-5-31')
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
